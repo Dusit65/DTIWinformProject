5149ae7112b4442521b63fae17fb769a66b88943
Dusit Pakdekunakorn <S6552410011@sau.ac.th>
2025-02-06 15:47:29 +0700

    Fix add semi colon

--- DTIWinformProject/Program.cs
+++ DTIWinformProject/Program.cs
@@ -15,7 +15,7 @@ namespace DTIWinformProject
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false)
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FrmSAURegister());
 
         }
